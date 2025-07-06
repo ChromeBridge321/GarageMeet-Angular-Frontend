39d0075ca1d6cc78c5be172a3bf6b09665d70b54
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-07-05 20:03:54 -0600

    cambios a la navbar de landingpage

--- src/features/presentation/presentation.component.ts
+++ src/features/presentation/presentation.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../shared/components/nav/nav.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-presentation',
-  imports: [NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent, RouterLink],
   templateUrl: './presentation.component.html',
 })
 export class PresentationComponent { }
