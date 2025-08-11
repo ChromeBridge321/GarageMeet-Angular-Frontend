172bac934199a656c4b25fd773bcc2f9292d89bb
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-08-11 07:11:39 -0600

    diseño dialog ventas

--- src/eviroments/enviroments.ts
+++ src/eviroments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://127.0.0.1:8000/api',
-    domainUrl: 'http://127.0.0.1:8000',
+    production: true,
+    apiUrl: 'https://garagemeet.site/api',
+    domainUrl: 'https://garagemeet.site',
     stripePublishableKey: 'pk_test_51RqxPyPlCUIY9G9QSqCypLJICRQgY5k6iP1WD5Po8X4OYgsMwfif8wL5rcW76pubYFx630gNtlW686pqB8yK2wYj00TxtibWDq'
 };
