564079c39276ae027d3c2da3f24bf48351c77cee
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-08-12 00:07:18 -0600

    adicion de los lins de descarga a la app movil

--- src/eviroments/enviroments.ts
+++ src/eviroments/enviroments.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.garagemeet.site/api',
   domainUrl: 'https://api.garagemeet.site',
   stripePublishableKey: 'pk_test_51RqxPyPlCUIY9G9QSqCypLJICRQgY5k6iP1WD5Po8X4OYgsMwfif8wL5rcW76pubYFx630gNtlW686pqB8yK2wYj00TxtibWDq',
-  downloadUrl: 'https://api.garagemeet.site/api/downloads/mobile-app'
+  downloadUrl: 'https://github.com/ChromeBridge321/GarageMeet-Angular-Frontend/releases/download/v1.m.0/GarageMeetMobile.apk'
 };
