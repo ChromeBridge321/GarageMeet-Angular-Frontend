9f2a4a2c6b999d34f40941aae32ea51654af20fa
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-10-24 14:58:49 -0600

    production data

--- src/eviroments/enviroments.ts
+++ src/eviroments/enviroments.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api',
-  domainUrl: 'http://127.0.0.1:8000',
-  stripePublishableKey: '',
+  production: true,
+  apiUrl: 'https://api.garagemeet.site/api',
+  domainUrl: 'https://api.garagemeet.site',
+  stripePublishableKey: 'pk_test_51RqxPgBAljMKdw0PMTAQeJrE5yDQzj99aJVpBlnBRlhWRYplRB9OwsG5PKSAcSPxppbVivDEIhO3BmEs3ytnaeHN00DeMZeasn',
   downloadUrl: ''
 };
